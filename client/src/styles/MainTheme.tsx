33dcd51312081e756615a7deae422455bcdae9a1
Sunny Eyles <sunnyeyles@Sunnys-MacBook-Air.local>
2023-09-14 22:04:52 +0200

    Added notes and an example of how to implement dark/light theme toggle

--- client/src/styles/MainTheme.tsx
+++ client/src/styles/MainTheme.tsx
@@ -1,18 +1,30 @@
 import { MantineProvider } from "@mantine/core";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 interface IMainTheme {
   children: ReactNode;
 }
 
 export const MainTheme = ({ children }: IMainTheme) => {
+  // this example is with useState(), we would use redux to hold the theme state in practice
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  // fancy looking mantine component we can use for the toggle button: https://v3.mantine.dev/core/switch/
+  // for now we can just keep the default mantine dark theme, maybe tweak it slightly at the end, probably don't even need to though
+
+  // simple function to toggle between light/dark theme
+  // the function will trigger onClick()
+  const toggleTheme = () => {
+    setIsDarkTheme((prevTheme) => !prevTheme);
+  };
+
   return (
     <MantineProvider
       withGlobalStyles
       withNormalizeCSS
       inherit
       theme={{
-        colorScheme: "light",
+        colorScheme: isDarkTheme ? "dark" : "light",
         fontFamily: "monospace",
 
         shadows: {
@@ -30,14 +42,13 @@ export const MainTheme = ({ children }: IMainTheme) => {
         components: {
           //Button styles
           Button: {
-            defaultProps: {
-            },
+            defaultProps: {},
             styles: {
               root: {
-                '&:focus-within': {
+                "&:focus-within": {
                   backgroundColor: "#f5901c",
                 },
-                '&:hover': {
+                "&:hover": {
                   backgroundColor: "#B14630",
                 },
                 // Add custom styles here
@@ -52,18 +63,18 @@ export const MainTheme = ({ children }: IMainTheme) => {
               },
             },
           },
-          
+
           Input: {
             defaultProps: {
-              variant: 'filled',
+              variant: "filled",
             },
             styles: {
-              input: { 
-                '&:focus-within': {
+              input: {
+                "&:focus-within": {
                   borderColor: "#f5901c",
                 },
                 backgroundColor: "#f7e3cb",
-                boxShadow: "5px 5px #f5901c"
+                boxShadow: "5px 5px #f5901c",
               },
             },
           },
