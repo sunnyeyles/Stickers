fb1d08db423c3c712ba14529ececf18bfcb5825a
Sonja <sonneumayer@gmail.com>
2023-09-18 17:06:50 +0200

    implements dashboard header and adds image

--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -1,13 +1,35 @@
-import { Link } from 'react-router-dom'
-import { Paper } from '@mantine/core'
+import { useNavigate } from 'react-router-dom'
+import { Overlay, Container, Title, Button, Text } from '@mantine/core';
+import { useStyles } from './dashboard_styles';
+
 
 export function Dashboard() {
-    return(
-        <>
-        <Paper ml={200}>
-            <h1>Dashboard</h1>
-            <Link to="/login">LOGIN</Link>
-        </Paper>
-        </>
-    )
+  const { classes } = useStyles();
+  const navigate = useNavigate();
+
+  const navigateToLogin = () => {
+    navigate('/login')
+  }
+  return (
+    <>
+      <div className={classes.hero}>
+        <Overlay
+          gradient="linear-gradient(180deg, rgba(247,227,203,0.25) 0%, rgba(148,199,192,0.5) 40%)"
+          opacity={1}
+          zIndex={0}
+        />
+        <Container className={classes.container}>
+          <Title className={classes.title}>STICKERS</Title>
+          <Text className={classes.description} size="xl" mt="xl">
+            Funny drawings characterised by ...
+          </Text>
+
+          <Button onClick={navigateToLogin} size="xl" radius="xl" className={classes.control}>
+            Login
+          </Button>
+        </Container>
+      </div>
+    </>
+  );
 }
+
