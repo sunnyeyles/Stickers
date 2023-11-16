9d0df93b3413d4ed9dc89d730c6d448c5617cf8b
Sunny Eyles <sunnyeyles@Sunnys-MacBook-Air.local>
2023-11-16 12:51:11 +0100

    Seperated item card into it's own component

--- client/src/pages/products/Products.tsx
+++ client/src/pages/products/Products.tsx
@@ -1,12 +1,22 @@
-import { ItemsCollection } from '../../components/itemsCollection/ItemsCollection'
+import { Card, Text, Title } from '@mantine/core'
+import { useGetAllItemsQuery } from '../../app/features/items/itemsApiSlice'
+import { ItemCard } from '../../components/itemCard/ItemCard'
 
-export function Products() {
-  
+export const Products = () => {
+  const { data: items } = useGetAllItemsQuery()
+
   return (
     <>
-      <div>
-        <ItemsCollection />
-      </div>
+      <Title size="h1" pt="xl" pb="lg" align="center">
+        COLLECTION
+      </Title>
+      {items?.length === 0 ? (
+        <Card>
+          <Text>No items at the moment</Text>
+        </Card>
+      ) : (
+        <ItemCard />
+      )}
     </>
   )
 }
